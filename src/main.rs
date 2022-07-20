e747e7eb0a21065138949b4408ab5e2488f0b97c
zzlk <2418897+zzlk@users.noreply.github.com>
2022-07-20 12:06:06 -0700

    restore printing for cli tool

--- src/main.rs
+++ src/main.rs
@@ -29,14 +29,14 @@ fn main() -> Result<()> {
         .reduce(|x, y| x.combine(&y).unwrap())
         .unwrap();
 
-    let wave = Wave2::new(width as isize, height as isize, &rules).logical_conclusion(
+    let wave = Wave2::new(width as isize, height as isize, &rules, None).logical_conclusion(
         &|x| {
-            // x.print_wave();
-            // info!(
-            //     "non-null tiles: {:6} / {:6}",
-            //     x.render().into_iter().filter(|x| *x != 15).count(),
-            //     width * height
-            // );
+            x.print_wave();
+            info!(
+                "non-null tiles: {:6} / {:6}",
+                x.render().into_iter().filter(|x| *x != 15).count(),
+                width * height
+            );
         },
         500,
         4,
